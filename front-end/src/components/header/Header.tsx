fb2b012de86199edfe34ca2452bca28ee2c10cb6
martincorona007 <martincorona007@hotmail.com>
2022-09-22 17:23:37 -0500

    front-end add input style

--- front-end/src/components/header/Header.tsx
+++ front-end/src/components/header/Header.tsx
@@ -14,8 +14,11 @@ function Header() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <input className="form-control me-2 ms-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <div className="input-group">
+            <input className="form-control  border-success ms-2 custom-search-input color-b" type="search" placeholder="Search" aria-label="Search"></input>
+            <button className="btn btn-success custom-search-button" type="submit">Search</button>
+          </div>
+          
         </form>
        
 
